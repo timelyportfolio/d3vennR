986d47914ce9071b8ae6ba4848694ba2b9abde42
timelyportfolio <kent.russell@timelyportfolio.com>
2015-06-05 11:09:33 -0500

    add tooltip to VennDiagram example

--- inst/examples.R
+++ inst/examples.R
@@ -422,4 +422,63 @@ d3.scale.category10().range(%s.map(function(col){return eval(col)}))
   ,layoutFunction = '
 function(d) { return venn.venn(d, { initialLayout: venn.classicMDSLayout });}
   '
+  , tasks = list(
+      htmlwidgets::JS('
+function(){
+var div = d3.select(this);
+
+// add a tooltip
+var tooltip = d3.select("body").append("div")
+.attr("class", "venntooltip")
+.style("position", "absolute")
+.style("text-align", "center")
+.style("width", 128)
+.style("height", 16)
+.style("background", "#333")
+.style("color","#ddd")
+.style("padding","2px")
+.style("border","0px")
+.style("border-radius","8px")
+.style("opacity",0);
+
+div.selectAll("path")
+.style("stroke-opacity", 0)
+.style("stroke", "#fff")
+.style("stroke-width", 0)
+
+// add listeners to all the groups to display tooltip on mousover
+div.selectAll("g")
+.on("mouseover", function(d, i) {
+
+// sort all the areas relative to the current item
+venn.sortAreas(div, d);
+
+// Display a tooltip with the current size
+tooltip.transition().duration(400).style("opacity", .9);
+tooltip.text(d.size + " users");
+
+// highlight the current path
+var selection = d3.select(this).transition("tooltip").duration(400);
+selection.select("path")
+.style("stroke-width", 3)
+.style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+.style("stroke-opacity", 1);
+})
+
+.on("mousemove", function() {
+tooltip.style("left", (d3.event.pageX) + "px")
+.style("top", (d3.event.pageY - 28) + "px");
+})
+
+.on("mouseout", function(d, i) {
+tooltip.transition().duration(400).style("opacity", 0);
+var selection = d3.select(this).transition("tooltip").duration(400);
+selection.select("path")
+.style("stroke-width", 0)
+.style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+.style("stroke-opacity", 0);
+});
+}
+    ')
+  )
 )
